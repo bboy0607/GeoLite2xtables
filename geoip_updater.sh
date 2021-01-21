2d687e8377875271f990c9b4b63f39fe51945ba9
Hank Tsai <bboy0607@gmail.com>
2021-01-21 15:25:04 +0800

    add mmdb

--- geoip_updater.sh
+++ geoip_updater.sh
@@ -1,6 +1,7 @@
 #!/bash
 cd /usr/local/src/GeoLite2xtables/
 ./00_download_geolite2
+#./00_download_geolite2_mmdb
 ./10_download_countryinfo
 cat /tmp/GeoLite2-Country-Blocks-IPv4.csv |./20_convert_geolite2 /tmp/CountryInfo.txt > /usr/share/xt_geoip/GeoIP-legacy.csv
 /usr/lib/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip /usr/share/xt_geoip/GeoIP-legacy.csv
